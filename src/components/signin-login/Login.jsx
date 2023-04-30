4bf050d7a0d94208cec7c196f6bedd595d9393f2
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-05-01 00:49:01 +0600

    handle signUP

--- src/components/signin-login/Login.jsx
+++ src/components/signin-login/Login.jsx
@@ -4,24 +4,37 @@ const Login = () => {
 
 
     return (
-        <div>
+        <>
             <h1 className="text-5xl text-center font-bold text-white">Login now!</h1>
-            <form className="w-96 mx-auto mt-10 mb-5 backdrop-blur-lg rounded-2xl border-2 border-white shadow-lg shadow-white p-8">
-                <div className="form-control py-2">
-                    <label ><span className="text-white">Email</span></label>
-                    <input type="text" placeholder="email" className="input input-bordered" />
-                </div>
-                <div className="form-control py-2">
-                    <label ><span className="text-white">Password</span></label>
-                    <input type="text" placeholder="password" className="input input-bordered" />
-                    <label ><a href="#" className="text-white link-hover">Forgot password?</a></label>
-                </div>
-                <div className="form-control py-2">
-                    <input className="btn btn-primary" type="submit" value="Log In" />
-                </div>
-                <h1 className="text-white text-center text-md font-semibold">If you new or dont have an ID, plz <Link to={'/register'} className="text-white link-hover">Sign Up</Link></h1>
-            </form>
-        </div>
+            <div className="w-96 mx-auto mt-10 mb-5 backdrop-blur-lg rounded-2xl border-2 border-white shadow-lg shadow-white p-8">
+                <form>
+                    <div className="form-control py-2">
+                        <label ><span className="text-white">Email</span></label>
+                        <input type="text" placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control py-2">
+                        <label ><span className="text-white">Password</span></label>
+                        <input type="text" placeholder="password" className="input input-bordered" />
+                        <label ><a href="#" className="text-white link-hover">Forgot password?</a></label>
+                    </div>
+                    <div className="form-control py-2">
+                        <input className="btn btn-primary" type="submit" value="Log In" />
+                    </div>
+                </form>
+                <hr />
+                <h1 className="text-white text-lg font-semibold text-center py-1">Or you can also log in with: </h1>
+                <button className="mt-2 border-2 rounded-lg w-full text-center py-2 px-4 flex justify-center items-center hover:bg-sky-300 hover:bg-opacity-25 active:bg-opacity-50">
+                    <img className="w-6" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="" />
+                    <h1 className="text-white pl-2 text-lg">Log in with Google</h1>
+                </button>
+                <button className="my-4 border-2 rounded-lg w-full text-center py-2 px-4 flex justify-center items-center hover:bg-sky-300 hover:bg-opacity-25 active:bg-opacity-50">
+                    <img className="w-6" src="https://upload.wikimedia.org/wikipedia/en/0/04/Facebook_f_logo_%282021%29.svg" alt="" />
+                    <h1 className="text-white pl-2 text-lg">Log in with Facebook</h1>
+                </button>
+                <hr />
+                <h1 className="text-white text-center text-md font-semibold mt-4">If you new or dont have an ID, plz <Link to={'/register'} className="text-white link-hover">Sign Up</Link></h1>
+            </div>
+        </>
     );
 };
 
--- src/components/signin-login/SignUp.jsx
+++ src/components/signin-login/SignUp.jsx
@@ -1,32 +1,54 @@
 import {Link} from "react-router-dom";
 
 const SignUp = () => {
+
+    const handleSignUp = event => {
+        event.preventDefault()
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, email, password);
+    }
+
     return (
-        <div>
+        <>
             <h1 className="text-5xl text-center font-bold text-white">Sign Up in Nagib Travels...!</h1>
-            <form className="w-96 mx-auto mt-10 mb-5 backdrop-blur-lg rounded-2xl border-2 border-white shadow-lg shadow-white p-8">
-                <div className="form-control py-2">
-                    <label><span className="text-white">Full Name</span></label>
-                    <input name="name" type="text" placeholder="Your Name" className="input input-bordered" required/>
-                </div>
-                <div className="form-control py-2">
-                    <label><span className="text-white">Email</span></label>
-                    <input name="email" type="email" placeholder="email" className="input input-bordered" required/>
-                </div>
-                <div className="form-control py-2">
-                    <label><span className="text-white">Password</span></label>
-                    <input name="password" type="password" placeholder="password" className="input input-bordered" required/>
-                </div>
-                <div className="form-control py-2">
-                    <label><span className="text-white">Confirm Password</span></label>
-                    <input name="confirm" type="password" placeholder="password" className="input input-bordered" required/>
-                </div>
-                <div className="form-control py-2">
-                    <input name="submit" className="btn btn-primary" type="submit" value="Sign Up" />
-                </div>
-                <h1 className="text-white text-center text-md font-semibold">If you have an ID, plz <Link to={'/login'} className="text-white link-hover">Log In</Link></h1>
-            </form>
-        </div>
+            <div className="w-96 mx-auto mt-10 mb-5 backdrop-blur-lg rounded-2xl border-2 border-white shadow-lg shadow-white p-8">
+                <form onSubmit={handleSignUp}>
+                    <div className="form-control py-2">
+                        <label><span className="text-white">Full Name</span></label>
+                        <input name="name" type="text" placeholder="Your Name" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control py-2">
+                        <label><span className="text-white">Email</span></label>
+                        <input name="email" type="email" placeholder="email" className="input input-bordered" autoComplete="current-email" required />
+                    </div>
+                    <div className="form-control py-2">
+                        <label><span className="text-white">Password</span></label>
+                        <input name="password" type="password" placeholder="password" className="input input-bordered" autoComplete="new-password" required />
+                    </div>
+                    <div className="form-control py-2">
+                        <label><span className="text-white">Confirm Password</span></label>
+                        <input name="confirm" type="password" placeholder="password" className="input input-bordered" autoComplete="new-password" required />
+                    </div>
+                    <div className="form-control py-2">
+                        <input name="submit" className="btn btn-primary" type="submit" value="Sign Up" />
+                    </div>
+                    <h1 className="text-white text-center text-md font-semibold py-1">If you have an ID, plz <Link to={'/login'} className="text-white link-hover">Log In</Link></h1>
+                </form>
+                <hr />
+                <h1 className="text-white text-lg font-semibold text-center py-1">Or you can also sign up with: </h1>
+                <button className="mt-2 border-2 rounded-lg w-full text-center py-2 px-4 flex justify-center items-center hover:bg-sky-300 hover:bg-opacity-25 active:bg-opacity-50">
+                    <img className="w-6" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="" />
+                    <h1 className="text-white pl-2 text-lg">Sign up with Google</h1>
+                </button>
+                <button className="mt-4 border-2 rounded-lg w-full text-center py-2 px-4 flex justify-center items-center hover:bg-sky-300 hover:bg-opacity-25 active:bg-opacity-50">
+                    <img className="w-6" src="https://upload.wikimedia.org/wikipedia/en/0/04/Facebook_f_logo_%282021%29.svg" alt="" />
+                    <h1 className="text-white pl-2 text-lg">Sign up with Facebook</h1>
+                </button>
+            </div>
+        </>
     );
 };
 
